eb3526f2316218ba5e88b356a7948087990158c7
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-04-06 21:24:11 +0300

    add get_oldest_message

--- tool.py
+++ tool.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 def load_json(file_path: str = 'result.json') -> dict:
     """
@@ -41,3 +42,50 @@ def chat_info(data: dict) -> dict:
     }
 
     return chat_info
+
+def get_oldest_message(data: dict) -> dict:
+    """
+    Retrieves the oldest message from the JSON data.
+
+    Args:
+    - data (dict): The JSON data.
+
+    Returns:
+    - oldest_message (dict): Dictionary containing the oldest message with full timestamp.
+    """
+    
+    oldest_message = {'date': '9999-12-31T23:59:59'}
+    
+    for message in data.get('messages', []):
+        if 'date' in message and message['date'] < oldest_message['date']:
+            oldest_message = message
+
+    oldest_message['date'] = extract_date_info(oldest_message)
+
+    return oldest_message
+
+
+def extract_date_info(message: dict) -> dict:
+    """
+    Extract date information from the message and return it as a dictionary.
+
+    Args:
+    - message (dict): The message containing the date information.
+
+    Returns:
+    - date_info (dict): Dictionary containing the extracted date information.
+    """
+    date_str = message.get('date')
+    date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+
+    date_info = {
+        'year': date_obj.year,
+        'month': date_obj.month,
+        'day': date_obj.day,
+        'hour': date_obj.hour,
+        'minute': date_obj.minute,
+        'second': date_obj.second,
+        'time': date_obj.strftime('%H:%M:%S')
+    }
+
+    return date_info
