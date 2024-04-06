35776c1627b8486d3f55f2ed0797b6c773966f08
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-04-06 20:10:55 +0300

    add chat_info

--- tool.py
+++ tool.py
@@ -18,3 +18,26 @@ def load_json(file_path: str = 'result.json') -> dict:
     except (FileNotFoundError, json.JSONDecodeError) as e:
         print(f"An error occurred while loading the JSON file: {e}")
         return None
+
+
+def chat_info(data: dict) -> dict:
+    """
+    Extract chat information from the JSON data.
+
+    Args:
+    - data (dict): The JSON data.
+
+    Returns:
+    - chat_info (dict): Dictionary containing chat information.
+    """
+    chat = data
+    messages_count = len(data.get('messages', []))
+
+    chat_info = {
+        'name': chat.get('name', 'Unknown'),
+        'type': chat.get('type', 'Unknown'),
+        'id': chat.get('id', 'Unknown'),
+        'messages_count': messages_count
+    }
+
+    return chat_info
