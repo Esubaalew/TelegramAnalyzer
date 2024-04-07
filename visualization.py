57d4d939e7ca8c93e201439671ab574140fd4f7c
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-04-07 09:58:26 +0300

    vis : new visual

--- visualization.py
+++ visualization.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 import numpy as np
-from tool import get_most_active_days, get_most_active_hours, get_most_active_users, get_most_active_weekdays, load_json, get_senders, each_average_message_length
+from tool import get_most_active_days, get_most_active_hours, get_most_active_months_by_year, get_most_active_users, get_most_active_weekdays, get_most_active_year, load_json, get_senders, each_average_message_length
 
 def visualize_most_active_users(data: dict, top_n: int = 10):
     """
-    Visualize the top N most active users based on the number of messages they sent.
+    Visualize the top N most activ users based on the number of messages they sent.
 
     Args:
     - data (dict): The JSON data.
@@ -225,5 +226,70 @@ def visualize_most_active_weekdays_pie(data: dict):
     plt.show()
 
 
-data = load_json('result.json')
-visualize_most_active_weekdays_pie(data)
+def visualize_message_activity_over_year(data: dict):
+    """
+    Visualize the change in message activity over time using a line plot.
+
+    Args:
+    - data (dict): The JSON data from the Telegram group export.
+    """
+    active_years_data = get_most_active_year(data)
+   
+    sorted_years = sorted(active_years_data)
+    years, message_counts = zip(*sorted_years)
+
+
+    # Plotting
+    plt.figure(figsize=(10, 5))
+    plt.plot(years, message_counts, marker='o')
+    plt.title('Number of Messages Over Time')
+    plt.xlabel('Year')
+    plt.ylabel('Number of Messages')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
+
+def visualize_most_active_months_by_year(data: dict):
+    """
+    Visualize the most active months in the Telegram group for each year using a grouped bar plot and export the data.
+
+    Args:
+    - data (dict): The JSON data from the Telegram group export.
+    - export_excel (bool): Whether to export the data to an Excel file. Defaults to True.
+    - export_json (bool): Whether to export the data to a JSON file. Defaults to True.
+    """
+   
+    active_months_by_year = get_most_active_months_by_year(data)
+
+    
+    years = sorted(active_months_by_year.keys())
+    month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    num_months = len(month_names)
+    bar_width = 0.35
+    index = np.arange(num_months)
+
+    
+    for year, months_data in active_months_by_year.items():
+        if len(months_data) < num_months:
+            missing_months = set(month_names) - set(month['name'] for month in months_data)
+            for month in missing_months:
+                active_months_by_year[year].append({'name': month, 'messages': 0})
+        active_months_by_year[year] = sorted(active_months_by_year[year], key=lambda x: month_names.index(x['name']))
+
+    
+    plt.figure(figsize=(12, 8))
+
+    for i, year in enumerate(years):
+        months_data = active_months_by_year[year]
+        month_counts = [month['messages'] for month in months_data]
+        plt.bar(index + i * bar_width, month_counts, bar_width, label=year)
+
+    plt.xlabel('Month')
+    plt.ylabel('Message Count')
+    plt.title('Most Active Months by Year')
+    plt.xticks(index + bar_width * len(years) / 2, month_names)
+    plt.legend(title='Year')
+    plt.tight_layout()
+    plt.show()
